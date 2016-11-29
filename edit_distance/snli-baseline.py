f49aa79864a3421ce9bd2a4196b5b4d3861234c3
danielvarab <djam@itu.dk>
2016-11-29 14:32:53 +0100

    added abit of pandas

--- edit_distance/snli-baseline.py
+++ edit_distance/snli-baseline.py
@@ -1,21 +1,46 @@
+import pandas as pd
 import numpy as np
 from sklearn import linear_model
 
+delete_cost = 1
+insertion_cost = 1
+substitution_cost = 1
+
+def delete(value): return value + delete_cost
+def insert(value): return value + insertion_cost
+
+def substitute(equal, value):
+    if equal:
+        return value
+    else:
+        return value+substitution_cost
+
 def edit_distance(s1, s2):
+    s1 = s1.split(" ")
+    s2 = s2.split(" ")
+
     m=len(s1)+1
     n=len(s2)+1
 
     tbl = {}
-    for i in range(m): tbl[i,0]=i
-    for j in range(n): tbl[0,j]=j
+    for i in range(m): tbl[i,0] = i
+    for j in range(n): tbl[0,j] = j
     for i in range(1, m):
         for j in range(1, n):
-            cost = 0 if s1[i-1] == s2[j-1] else 1
-            tbl[i,j] = min(tbl[i, j-1]+1, tbl[i-1, j]+1, tbl[i-1, j-1]+cost)
+            d_cost = delete(tbl[i, j-1])
+            i_cost = insert(tbl[i-1, j])
+            s_cost = substitute(s1[i-1] == s2[j-1], tbl[i-1, j-1])
 
+            tbl[i,j] = min(d_cost, i_cost, s_cost)
+
+    # print(tbl)
     return tbl[i,j]
 
-trial = open('SICK_trial.txt', 'r')
+file_path = "/home/danielvarab/school/contradiction-detection/sick_dataset/SICK_train.txt"
+df = pd.read_csv(file_path, sep="\t")
+
+filtered = df[["sentence_A", "sentence_B", "entailment_judgment"]]
+
 data = np.loadtxt(trial)
 
 
