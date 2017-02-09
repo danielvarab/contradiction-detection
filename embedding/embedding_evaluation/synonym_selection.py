eacb717beb154ff1593a61896eb0cf253555b4be
danielvarab <djam@itu.dk>
2017-02-09 15:52:05 +0100

    cosine is the only relevant, so euclidian metric is removed.

--- embedding/embedding_evaluation/synonym_selection.py
+++ embedding/embedding_evaluation/synonym_selection.py
@@ -2,18 +2,16 @@ import numpy as np
 from ranking import *
 
 """ synonym_selection (TOEFL) """
-def syn_ant_selection(embedding, tasks, distance_metric="cosine"):
+def syn_ant_selection(embedding, tasks):
 	results = []
 	distance_func = None
 	mean_vector = np.mean(np.array(embedding.values()), axis=0)
 	for word, relations, gold in tasks:
 		placeholder = np.arange(300)
 		word_emb = embedding.get(word, mean_vector)
-		if distance_metric is "euclid":
-			distance_func = lambda x: euclidean(embedding.get(x, mean_vector), word_emb)
-		if distance_metric is "cosine":
-			distance_func = lambda x: cosine_sim(embedding.get(x, mean_vector), word_emb)
-		index = np.argmax(map(distance_func, relations))
+		distance_func = lambda x: cosine_sim(embedding.get(x, mean_vector), word_emb)
+		index = np.argmin(map(distance_func, relations))
+
 		if relations[index] == gold:
 			results.append(1)
 		else:
