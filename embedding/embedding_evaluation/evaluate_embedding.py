56d9212106789d2be9936f0f17581343518fb324
danielvarab <djam@itu.dk>
2017-02-09 10:14:50 +0100

    fixed relative data folder and modified calling call to synonym_antonym

--- embedding/embedding_evaluation/evaluate_embedding.py
+++ embedding/embedding_evaluation/evaluate_embedding.py
@@ -4,9 +4,13 @@ import argparse
 import numpy as np
 from eval_sentiment import load_sentiment_data, test_embedding_on_task
 from word_simularity import eval_all_sim
-from synonym_selection import synonym_selection
+from synonym_selection import syn_ant_selection
 from syntactic_relation import *
 from read_write import *
+
+def rel_path(path):
+ return os.path.join(os.path.dirname(__file__), path)
+
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
@@ -29,7 +33,8 @@ if __name__ == "__main__":
 		print('======================================')
 		print('Word Simularity Evaluations')
 		print('======================================')
-		eval_all_sim(embedding, "./word_sim_tasks")
+		ws_path = rel_path('word_sim_tasks')
+		eval_all_sim(embedding, ws_path)
 	else:
 		print(">> Skipped Word Similarity")
 
@@ -38,8 +43,9 @@ if __name__ == "__main__":
 		print('======================================')
 		print('Synonym/Antonym Selection Evaluation')
 		print('======================================')
-		tasks = load_toefle("./synonym_selection_tasks/testset950.txt")
-		precision = synonym_selection(embedding, tasks)
+		gre_path = rel_path("synonym_selection_tasks/testset950.txt")
+		tasks = load_toefle(gre_path)
+		precision = syn_ant_selection(embedding, tasks)
 		print("> {0}: \t{1} (accuracy) - skipped {2}/{3}".format("testset950 (GRE)", precision, "TODO", "uses mean vector"))
 	else:
 		print(">> Skipped Synonym Selection (TOEFL)")
@@ -50,7 +56,9 @@ if __name__ == "__main__":
 		print('======================================')
 		print('Sentiment Analysis Evaluation')
 		print('======================================')
-		SA_score = test_embedding_on_task(embedding, "./sentiment_analysis/train.txt", "./sentiment_analysis/test.txt")
+		sa_train_path = rel_path("sentiment_analysis/train.txt")
+		sa_test_path = rel_path("sentiment_analysis/test.txt")
+		SA_score = test_embedding_on_task(embedding, sa_train_path, sa_test_path)
 		print("> {0}: \t{1} (accuracy) - skipped {2}/{3}".format("stanford-tree", SA_score, "TODO", "(uses mean vector)"))
 		print("\n")
 
