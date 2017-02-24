83190aeb26e668b00817ad618664951560ed9d30
helvind <jakob.helvind@gmail.com>
2017-02-24 15:35:45 +0100

    small script for finding word with most antonyms and synonyms

---
+++ embedding/evaluation/count_syns_ants.py
@@ -0,0 +1,55 @@
+import argparse
+import sys
+import os
+from read_write import read_lexicon
+
+
+def count_syns_ants(synonyms, antonyms, vocab):
+	max_word = ""
+	min_word = ""
+	max_count = 0
+	min_count = 0
+	if vocab is None:
+		vocab = set(synonyms.keys()).intersection(set(antonyms.keys()))
+	else:
+		vocab = set(synonyms.keys()).intersection(set(antonyms.keys())).intersection(set(vocab))
+
+	for word in vocab:
+		length = len(synonyms[word])+len(antonyms[word])
+		min_length = min(len(synonyms[word]),len(antonyms[word]))
+		if (length > max_count):
+			max_count = length
+			max_word = word
+		if (min_length > min_count):
+			min_count = min_length
+			min_word = word
+
+	return max_word, max_count, min_word, min_count 
+
+
+if __name__=='__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--s', required=True, help="synonym file")
+	parser.add_argument('--a', required=True, help="antonym file")
+	parser.add_argument('--e', required=False, help="optional word embedding for filtering")
+	args = parser.parse_args(sys.argv[1:])
+
+	synonyms = read_lexicon(args.s)
+	antonyms = read_lexicon(args.a)
+
+	vocab = None
+	if args.e is not None:
+		with Open(args.e,"r") as f:
+    		vocab = [r.split()[0] for r in f]
+
+	max_word, max_count, min_word, min_count  = count_syns_ants(synonyms, antonyms, vocab)
+
+
+	print max_word
+	print max_count
+	print len(synonyms[max_word])
+	print len(antonyms[max_word])
+	print min_word
+	print min_count
+	print len(synonyms[min_word])
+	print len(antonyms[min_word])
