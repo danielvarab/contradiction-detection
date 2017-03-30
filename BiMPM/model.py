cca4d29b1bc9b129ab4823d4c57cf9a99be2a6d7
danielvarab <djam@itu.dk>
2017-03-30 13:21:30 +0200

    added dropout and increased batch size

--- BiMPM/model.py
+++ BiMPM/model.py
@@ -1,4 +1,4 @@
-from keras.layers import Input, LSTM, Dense, Embedding, merge, Bidirectional, Lambda, Flatten, Reshape, TimeDistributed
+from keras.layers import Input, LSTM, Dense, Embedding, merge, Bidirectional, Lambda, Flatten, Reshape, TimeDistributed, Dropout
 from keras.models import Model
 from keras.engine.topology import Layer
 from keras import backend as K
@@ -126,50 +126,49 @@ def build_model_2(char_vocab_size, sentence_length, word_length):
     char_a_input = Input(shape=(sentence_length, word_length), name="char_sentence_A")
     char_b_input = Input(shape=(sentence_length, word_length), name="char_sentence_B")
 
-    with tf.device('/gpu:3'):
-        char_embedding = TimeDistributed(Embedding(char_vocab_size, 20, input_length=word_length))
+    char_embedding = TimeDistributed(Embedding(char_vocab_size, 20, input_length=word_length))
 
-        char_a_embs = char_embedding(char_a_input)
-        char_b_embs = char_embedding(char_b_input)
+    char_a_embs = char_embedding(char_a_input)
+    char_b_embs = char_embedding(char_b_input)
 
-        char_lstm = TimeDistributed(LSTM(50))
+    char_lstm = TimeDistributed(LSTM(50))
 
-        char_sentence_a = char_lstm(char_a_embs)
-        char_sentence_b = char_lstm(char_b_embs)
+    char_sentence_a = char_lstm(char_a_embs)
+    char_sentence_b = char_lstm(char_b_embs)
 
-        sentence_a_emb = merge([word_a_input, char_sentence_a], mode='concat', name="sentence_a_emb")
-        sentence_b_emb = merge([word_b_input, char_sentence_b], mode='concat', name="sentence_b_emb")
+    sentence_a_emb = merge([word_a_input, char_sentence_a], mode='concat', name="sentence_a_emb")
+    sentence_b_emb = merge([word_b_input, char_sentence_b], mode='concat', name="sentence_b_emb")
 
     # one direction
     fw_a_ctx = LSTM(100, return_sequences=True)(sentence_a_emb)
     fw_b_ctx = LSTM(100, return_sequences=True)(sentence_b_emb)
 
-    s_pair = [fw_a_ctx, fw_b_ctx]
-    r_s_pair = [fw_b_ctx, fw_a_ctx]
+    fw_a_ctx = Dropout(.1)(fw_a_ctx)
+    fw_b_ctx = Dropout(.1)(fw_b_ctx)
 
-    with tf.device('/gpu:2'):
-        pq_match_1 = FullMatch()(s_pair)
-        pq_match_2 = MaxPoolingMatch()(s_pair)
-        pq_match_3 = AttentiveMatch()(s_pair)
-        pq_match_4 = MaxAttentiveMatch()(s_pair)
+    pq_match_1 = FullMatch()([fw_a_ctx, fw_b_ctx])
+    pq_match_2 = MaxPoolingMatch()([fw_a_ctx, fw_b_ctx])
+    pq_match_3 = AttentiveMatch()([fw_a_ctx, fw_b_ctx])
+    pq_match_4 = MaxAttentiveMatch()([fw_a_ctx, fw_b_ctx])
 
-        qp_match_1 = FullMatch()(r_s_pair)
-        qp_match_2 = MaxPoolingMatch()(r_s_pair)
-        qp_match_3 = AttentiveMatch()(r_s_pair)
-        qp_match_4 = MaxAttentiveMatch()(r_s_pair)
+    qp_match_1 = FullMatch()([fw_b_ctx, fw_a_ctx])
+    qp_match_2 = MaxPoolingMatch()([fw_b_ctx, fw_a_ctx])
+    qp_match_3 = AttentiveMatch()([fw_b_ctx, fw_a_ctx])
+    qp_match_4 = MaxAttentiveMatch()([fw_b_ctx, fw_a_ctx])
 
-    with tf.device('/gpu:3'):
+    pq_combined_matches = merge([pq_match_1, pq_match_2, pq_match_3, pq_match_4], mode="concat", concat_axis=1)
+    qp_combined_matches = merge([qp_match_1, qp_match_2, qp_match_3, qp_match_4], mode="concat", concat_axis=1)
 
-        pq_combined_matches = merge([pq_match_1, pq_match_2, pq_match_3, pq_match_4], mode="concat", concat_axis=1)
-        qp_combined_matches = merge([qp_match_1, qp_match_2, qp_match_3, qp_match_4], mode="concat", concat_axis=1)
+    p_to_q_fw_bw_aggr = Bidirectional(LSTM(100), merge_mode="concat")(pq_combined_matches)
+    q_to_p_fw_bw_aggr = Bidirectional(LSTM(100), merge_mode="concat")(qp_combined_matches)
 
-        p_to_q_fw_bw_aggr = Bidirectional(LSTM(100), merge_mode="concat")(pq_match_1)
-        q_to_p_fw_bw_aggr = Bidirectional(LSTM(100), merge_mode="concat")(qp_match_1)
+    p_to_q_fw_bw_aggr = Dropout(.1)(p_to_q_fw_bw_aggr)
+    q_to_p_fw_bw_aggr = Dropout(.1)(q_to_p_fw_bw_aggr)
 
-        matching_vector = merge([p_to_q_fw_bw_aggr, q_to_p_fw_bw_aggr], mode='concat')
+    matching_vector = merge([p_to_q_fw_bw_aggr, q_to_p_fw_bw_aggr], mode='concat')
 
-        prediction_layer = Dense(3, activation='softmax', name="output")(matching_vector)
+    prediction_layer = Dense(3, activation='softmax', name="output")(matching_vector)
 
-        model = Model(input=[word_a_input, char_a_input, word_b_input, char_b_input], output=prediction_layer)
+    model = Model(input=[word_a_input, char_a_input, word_b_input, char_b_input], output=prediction_layer)
 
     return model
