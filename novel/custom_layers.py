f77fd4b80c33277355b2922d0548ec649170b994
danielvarab <djam@itu.dk>
2017-04-25 15:46:21 +0200

    removed trainable weights and added lambda

--- novel/custom_layers.py
+++ novel/custom_layers.py
@@ -4,15 +4,9 @@ from keras.engine.topology import Layer
 class Align(Layer):
     def __init__(self, normalize=False, trainable=False, **kwargs):
         self.normalize = normalize
-        self.trainable = trainable
         super(Align, self).__init__(**kwargs)
 
     def build(self, input_shape):
-        a_input_shape, b_input_shape = input_shape
-
-        # Create a trainable weight variable for this layer.
-        if self.trainable is True:
-            self.kernel = self.add_weight(shape=(1, a_input_shape[2]), initializer='uniform', trainable=True)
         super(Align, self).build(input_shape)  # Be sure to call this somewhere!
 
     def call(self, x):
@@ -23,37 +17,16 @@ class Align(Layer):
             a = K.l2_normalize(a, axis=2)
             b = K.l2_normalize(b, axis=2)
 
-
-        if self.trainable:
-            a = a * self.kernel
-            b = b * self.kernel
-
         return K.batch_dot(a, b, axes=[2, 2])
 
     def compute_output_shape(self, input_shape):
         a_shape, b_shape = input_shape
         return (a_shape[0], a_shape[1], b_shape[1])
 
-# TODO: implement axis squash
-class Summarize(Layer):
-    def __init__(self, trainable=False, **kwargs):
-        self.trainable = trainable
-        super(Summarize, self).__init__(**kwargs)
+def max_both_directions(x):
+    left = K.max(x,axis=1)
+    right = K.max(x,axis=2)
+    return K.concatenate([left, right], axis=1)
 
-    def build(self, input_shape):
-        if self.trainable:
-            self.kernel = self.add_weight(shape=(input_shape[1], input_shape[2]), initializer='uniform', trainable=True)
-        super(Summarize, self).build(input_shape) # Be sure to call this somewhere!
-
-    def call(self, x):
-        x_shape = K.int_shape(x)
-        bow = x[:,0,:]
-        for i in range(1, x_shape[1]):
-            if self.trainable: word = self.kernel * x[:,i,:]
-            else : word = x[:,i,:]
-            bow = bow + word
-
-        return bow
-
-    def compute_output_shape(self, input_shape):
-        return (input_shape[0], input_shape[2])
+def max_both_directions_output_shape(input_shape):
+    return (input_shape[0], input_shape[1]*2)
