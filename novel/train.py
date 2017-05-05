8db62df58512da67d0e7cdd2d25ff25e86e41224
danielvarab <djam@itu.dk>
2017-05-05 11:44:26 +0200

    removed, moved, renamed

--- novel/train.py
+++ novel/train.py
@@ -13,14 +13,14 @@ from keras.models import Model
 from keras.utils import np_utils
 from keras.layers import Input, Dense, LSTM, TimeDistributed, Bidirectional, Flatten, Embedding, Lambda, concatenate, Dropout
 from keras.callbacks import EarlyStopping, ModelCheckpoint, LambdaCallback
-from keras.layers.merge import Concatenate, Dot, maximum
 from keras.layers.normalization import BatchNormalization
 from keras.regularizers import l2
 
 from keras.preprocessing.sequence import pad_sequences
 from keras.preprocessing.text import Tokenizer
 import keras.preprocessing.text
-from custom_layers import *
+from custom_layers import Align, Aggregate, _align, _aggregate
+# from callbacks import LossHistory
 from preprocess import get_embedding_matrix
 
 parser = argparse.ArgumentParser()
@@ -29,48 +29,48 @@ parser.add_argument('--dev', required=True, help="dev file. JSONL file plz")
 parser.add_argument('--test', required=True, help="snli test file. JSONL file plz")
 parser.add_argument('--embedding', required=True, help="embedding file")
 parser.add_argument('--ant_embedding', required=True, help="antnonym embedding file")
-parser.add_argument('--timedist', required=False, default=False, help='boolean for applying timedistributed layer after embedding')
 parser.add_argument('--align_op_we', required=False, help='operator used to sqaush sentence alignment matrix')
 parser.add_argument('--agg_we', required=False, help='operator for aggregating over sentence embeddings')
 parser.add_argument('--align_op_ae', required=False, help='operator used to sqaush antonym alignment matrix')
 parser.add_argument('--agg_ae', required=False, help='operator for aggregating over antonym embeddings')
+parser.add_argument('--outfile', required=False, help="File to write results in")
 parser.add_argument('--neurons', default=300, help='Number of hidden nodes')
 parser.add_argument('--patience', default=8, help='How long should I wait because breaking out?')
-parser.add_argument('--outfile', required=True, help="File to write results in")
+parser.add_argument('--timedist', required=False, default=False, help='boolean for applying timedistributed layer after embedding')
+
 
 args = parser.parse_args(sys.argv[1:])
-
-train_file = args.train
-dev_file = args.dev
-test_file = args.test
-emb_file = args.embedding
-ant_emb_file = args.ant_embedding
 
 start = timeit.default_timer()
 
 ## SETUP LOGGER
-outfile = args.outfile
+VERBOSE = 1
+if args.outfile:
+    print = logging.info
+    VERBOSE = 2
+    logging.basicConfig(filename=args.outfile, level=logging.INFO)
+    stdLogger=logging.StreamHandler(sys.stdout)
+    stdLogger.setFormatter(logging.Formatter(logging.BASIC_FORMAT))
 
-logging.basicConfig(filename=outfile, level=logging.INFO)
-stdLogger=logging.StreamHandler(sys.stdout)
-stdLogger.setFormatter(logging.Formatter(logging.BASIC_FORMAT))
+    logging.getLogger().addHandler(stdLogger)
 
-logging.getLogger().addHandler(stdLogger)
+class LossHistory(keras.callbacks.Callback):
+    def on_train_begin(self, logs={}):
+        print(" First epoch has been initiated")
+    def on_epoch_end(self, batch, logs={}):
+        print(str(logs))
+        loss = logs.get('loss')
+        val_acc = logs.get('val_acc')
+        acc = logs.get('acc')
+        print(" Epoch: " + str(batch) + "- ACC: " + str(acc) + " LOSS: " + str(loss) + " VAL_ACC: " + str(val_acc))
 
-logging.info('PARAMETERS:\n')
-logging.info('Embedding file: ' + emb_file)
-logging.info('Ant Embedding file: ' + ant_emb_file)
-logging.info('Neurons: ' + args.neurons)
-logging.info('Patience: ' + args.patience)
-logging.info('Timedistribution: ' + str(args.timedist))
-logging.info('Operators :')
-logging.info("\tWord Embedding Alignment OP: " + str(args.align_op_we))
-logging.info("\tWord Embedding Aggregation OP: " + str(args.agg_we))
-logging.info("\tAnt Embedding Alignment OP: " + str(args.align_op_ae))
-logging.info("\tAnt Embedding Aggregation OP: " + str(args.agg_ae) )
-
-logging.info('Arguments: ' + str(args) + '\n\n')
-
+print(' PARAMETERS:')
+print(' Embedding file: ' + args.embedding)
+print(' Ant Embedding file: {0}'.format(args.ant_embedding))
+print(" Word Embedding Alignment OP: {0}".format(args.align_op_we))
+print(" Word Embedding Aggregation OP: {0}".format(args.agg_we))
+print(" Ant Embedding Alignment OP: {0}".format(args.align_op_ae))
+print(" Ant Embedding Aggregation OP: {0}".format(args.agg_ae))
 
 def extract_tokens_from_binary_parse(parse):
     return parse.replace('(', ' ').replace(')', ' ').replace('-LRB-', '(').replace('-RRB-', ')').split()
@@ -93,9 +93,8 @@ def get_data(fn, limit=None):
     raw_data = list(yield_examples(fn=fn, limit=limit))
     left = [s1 for _, s1, s2 in raw_data]
     right = [s2 for _, s1, s2 in raw_data]
-    # words
-    logging.info(max(len(x.split()) for x in left))
-    logging.info(max(len(x.split()) for x in right))
+    print(" sentence length in {0}: {1}".format(fn,max(len(x.split()) for x in left)))
+    print(" sentence length in {0}: {1}".format(fn,max(len(x.split()) for x in right)))
 
     LABELS = {'contradiction': 0, 'neutral': 1, 'entailment': 2}
     Y = np.array([LABELS[l] for l, s1, s2 in raw_data])
@@ -103,197 +102,143 @@ def get_data(fn, limit=None):
 
     return left, right, Y
 
-training = get_data(train_file)
-validation = get_data(dev_file)
-test = get_data(test_file)
-
+training = get_data(args.train)
+validation = get_data(args.dev)
+test = get_data(args.test)
 
 tokenizer = Tokenizer(lower=False, filters='')
 tokenizer.fit_on_texts(training[0] + training[1])
 
-VOCAB_SIZE = len(tokenizer.word_counts) + 1
-SENTENCE_MAX_LEN = 42
-WORD_DIM = 300
-ANT_WORD_DIM = 200
-EPOCHS = 42
-PERSPECTIVES = 5
-CLASS_COUNT = 3
-PATIENCE = int(args.patience)
+# Lowest index from the tokenizer is 1 - we need to include 0 in our vocab count
+VOCAB = len(tokenizer.word_counts) + 1
+EMBED_HIDDEN_SIZE = 300
+SENT_HIDDEN_SIZE = 300
+ANT_SENT_HIDDEN_SIZE = 200
 BATCH_SIZE = 512
-DENSE_NEURON_COUNT = int(args.neurons)
+PATIENCE = 4 # 8
+MAX_EPOCHS = 42
+MAX_LEN = 42
 DP = 0.2
 L2 = 4e-6
+ACTIVATION = 'relu'
+OPTIMIZER = 'rmsprop'
 
-to_seq = lambda X: pad_sequences(tokenizer.texts_to_sequences(X), maxlen=SENTENCE_MAX_LEN)
+to_seq = lambda X: pad_sequences(tokenizer.texts_to_sequences(X), maxlen=MAX_LEN)
 prepare_data = lambda data: (to_seq(data[0]), to_seq(data[1]), data[2])
 
 training = prepare_data(training)
 validation = prepare_data(validation)
 test = prepare_data(test)
 
-logging.info("> fetching embedding")
-embedding_matrix = get_embedding_matrix(emb_file, VOCAB_SIZE, WORD_DIM, tokenizer)
+premise = Input(shape=(MAX_LEN,), dtype='int32')
+hypothesis = Input(shape=(MAX_LEN,), dtype='int32')
 
-logging.info("> fetching antonym embedding")
-ant_embedding_matrix = get_embedding_matrix(ant_emb_file, VOCAB_SIZE, ANT_WORD_DIM, tokenizer)
+# read in embedding and translate
+if args.agg_we != None or args.align_op_we != None:
+    print(" fetching word embedding")
+    embedding_matrix = get_embedding_matrix(args.embedding, VOCAB, EMBED_HIDDEN_SIZE, tokenizer)
+    embed = Embedding(VOCAB, EMBED_HIDDEN_SIZE, weights=[embedding_matrix], input_length=MAX_LEN, trainable=False)
 
-embed = Embedding(VOCAB_SIZE, WORD_DIM, weights=[embedding_matrix], input_length=SENTENCE_MAX_LEN, trainable=False)
-ant_embed = Embedding(VOCAB_SIZE, ANT_WORD_DIM, weights=[ant_embedding_matrix], input_length=SENTENCE_MAX_LEN, trainable=False)
+    prem = embed(premise)
+    hypo = embed(hypothesis)
 
-translate = TimeDistributed(Dense(WORD_DIM, activation='relu'))
-ant_translate = TimeDistributed(Dense(ANT_WORD_DIM, activation='relu'))
+    translate = TimeDistributed(Dense(SENT_HIDDEN_SIZE, activation=ACTIVATION))
 
-premise = Input(shape=(SENTENCE_MAX_LEN,), dtype='int32', name="sentence_a")
-hypothesis = Input(shape=(SENTENCE_MAX_LEN,), dtype='int32', name="sentence_b")
+    prem = translate(prem)
+    hypo = translate(hypo)
 
-s1 = embed(premise)
-s2 = embed(hypothesis)
+# read in antonym embedding and translate
+if args.agg_ae != None or args.align_op_ae != None:
+    print(" fetching antonym word embedding")
+    antonym_embedding_matrix = get_embedding_matrix(args.ant_embedding, VOCAB, ANT_SENT_HIDDEN_SIZE, tokenizer)
+    antonym_embed = Embedding(VOCAB, ANT_SENT_HIDDEN_SIZE, weights=[antonym_embedding_matrix], input_length=MAX_LEN, trainable=False)
 
-ant_s1 = ant_embed(premise)
-ant_s2 = ant_embed(hypothesis)
+    ant_prem = antonym_embed(premise)
+    ant_hypo = antonym_embed(hypothesis)
 
-if args.timedist:
-    logging.info ('Using time distribution')
-    s1 = translate(s1)
-    s2 = translate(s2)
+    antonym_translate = TimeDistributed(Dense(ANT_SENT_HIDDEN_SIZE, activation=ACTIVATION))
 
-    ant_s1 = ant_translate(ant_s1)
-    ant_s2 = ant_translate(ant_s2)
+    ant_prem = antonym_translate(ant_prem)
+    ant_hypo = antonym_translate(ant_hypo)
 
-if args.agg_we == 'SUM':
-    aggre_operation = Lambda(lambda x: K.sum(x, axis=1), output_shape=(WORD_DIM, ))
-elif args.agg_we == 'MAX':
-    aggre_operation = Lambda(lambda x: K.max(x, axis=1), output_shape=(WORD_DIM, ))
-elif args.agg_we == 'MIN':
-    aggre_operation = Lambda(lambda x: K.min(x, axis=1), output_shape=(WORD_DIM, ))
-elif args.agg_we == 'MEAN':
-    aggre_operation = Lambda(lambda x: K.mean(x, axis=1), output_shape=(WORD_DIM, ))
-elif args.agg_we is not None:
-    logging.info('Invalid operator for WE aggregation')
-    exit(1)
-
-if args.agg_ae == 'SUM':
-    ant_aggre_operation = Lambda(lambda x: K.sum(x, axis=1), output_shape=(ANT_WORD_DIM, ))
-elif args.agg_ae == 'MAX':
-    ant_aggre_operation = Lambda(lambda x: K.max(x, axis=1), output_shape=(ANT_WORD_DIM, ))
-elif args.agg_ae == 'MIN':
-    ant_aggre_operation = Lambda(lambda x: K.min(x, axis=1), output_shape=(ANT_WORD_DIM, ))
-elif args.agg_ae == 'MEAN':
-    ant_aggre_operation = Lambda(lambda x: K.mean(x, axis=1), output_shape=(ANT_WORD_DIM, ))
-elif args.agg_ae is not None:
-    logging.info('Invalid operator for AE aggregation')
-    exit(1)
-
-
-if args.align_op_we == 'SUM':
-    operation_aligner = Lambda(sum_both_directions, output_shape=both_directions_output_shape)
-elif args.align_op_we == 'MAX':
-    operation_aligner = Lambda(max_both_directions, output_shape=both_directions_output_shape)
-elif args.align_op_we == 'MIN':
-    operation_aligner = Lambda(min_both_directions, output_shape=both_directions_output_shape)
-elif args.align_op_we == 'MEAN':
-    operation_aligner = Lambda(mean_both_directions, output_shape=both_directions_output_shape)
-elif args.align_op_we is not None:
-    logging.info('Invalid operator for WE alignment')
-    exit(1)
-
-if args.align_op_ae == 'SUM':
-    ant_operation_aligner = Lambda(sum_both_directions, output_shape=both_directions_output_shape)
-elif args.align_op_ae == 'MAX':
-    ant_operation_aligner = Lambda(max_both_directions, output_shape=both_directions_output_shape)
-elif args.align_op_ae == 'MIN':
-    ant_operation_aligner = Lambda(max_both_directions, output_shape=both_directions_output_shape)
-elif args.align_op_ae == 'MEAN':
-    ant_operation_aligner = Lambda(mean_both_directions, output_shape=both_directions_output_shape)
-elif args.align_op_ae is not None:
-    logging.info('Invalid operator for AE alignment')
-    exit(1)
-
-tensor = []
-
+reps = [] # sentence representations
 if args.agg_we is not None:
-    logging.info('Using aggregation of WE with operator ' + args.agg_we)
-    agg1 = aggre_operation(s1)
-    agg2 = aggre_operation(s2)
-    if args.agg_we == 'SUM':
-        agg1 = BatchNormalization()(agg1)
-        agg2 = BatchNormalization()(agg2)
-    tensor.append(agg1)
-    tensor.append(agg2)
-
-if args.agg_ae is not None:
-    logging.info('Using aggregation of AE with operator ' + args.agg_ae)
-    ant_agg1 = ant_aggre_operation(ant_s1)
-    ant_agg2 = ant_aggre_operation(ant_s2)
-    if args.agg_ae == 'SUM':
-        ant_agg1 = BatchNormalization()(ant_agg1)
-        ant_agg2 = BatchNormalization()(ant_agg2)
-    tensor.append(ant_agg1)
-    tensor.append(ant_agg2)
+    prem_aggr = _aggregate(prem, args.agg_we, axis=1)
+    hypo_aggr = _aggregate(hypo, args.agg_we, axis=1)
+    reps.append(prem_aggr)
+    reps.append(hypo_aggr)
 
 if args.align_op_we is not None:
-    logging.info('Using alignment of WE with operator ' + args.align_op_we)
-    alignment = Align()([s1,s2])
-    if args.align_op_we == 'SUM':
-        alignment = BatchNormalization()(alignment)
-    tensor.append(operation_aligner(alignment))
+    alignment = _align(prem, hypo, normalize=True)
+    alignment_aggr_1 = _aggregate(alignment, args.align_op_we, axis=1)
+    alignment_aggr_2 = _aggregate(alignment, args.align_op_we, axis=2)
+    reps.append(alignment_aggr_1)
+    reps.append(alignment_aggr_2)
+
+if args.agg_ae is not None:
+    ant_prem_aggr = _aggregate(ant_prem, args.agg_ae, axis=1)
+    ant_hypo_aggr = _aggregate(ant_hypo, args.agg_ae, axis=1)
+    reps.append(ant_prem_aggr)
+    reps.append(ant_hypo_aggr)
 
 if args.align_op_ae is not None:
-    logging.info('Using alignment of AE with operator ' + args.align_op_ae)
-    alignment = Align()([ant_s1,ant_s2])
-    if args.align_op_ae == 'SUM':
-        alignment = BatchNormalization()(alignment)
-    tensor.append(ant_operation_aligner(alignment))
+    alignment = _align(ant_prem, ant_hypo, normalize=True)
+    alignment_aggr_1 = _aggregate(alignment, args.align_op_ae, axis=1)
+    alignment_aggr_2 = _aggregate(alignment, args.align_op_ae, axis=2)
+    reps.append(alignment_aggr_1)
+    reps.append(alignment_aggr_2)
 
-logging.info('tensor :' + str(tensor))
+assert len(reps) > 0, "no sentence representations, hence no output of the translation layer"
 
-if(len(tensor) == 0):
-    logging.info("Invalid length of tensor, quitting!")
-    exit(1)
-elif(len(tensor) == 1):
-    logging.info("list of tensor has only a single element")
-    aggregation = tensor[0]
-else:
-    aggregation = concatenate(tensor, axis=-1)
-
-logging.info('aggregation shape: ' + str(aggregation.get_shape()))
-
-joint = Dropout(DP)(aggregation)
+joint = concatenate(reps)
+joint = Dropout(DP)(joint)
 for i in range(3):
-    joint = Dense(DENSE_NEURON_COUNT, activation='relu', W_regularizer=l2(L2) if L2 else None)(joint)
+    joint = Dense(2 * SENT_HIDDEN_SIZE, activation=ACTIVATION, kernel_regularizer=l2(L2))(joint)
     joint = Dropout(DP)(joint)
     joint = BatchNormalization()(joint)
 
-prediction = Dense(CLASS_COUNT, activation='softmax')(joint)
+pred = Dense(3, activation='softmax')(joint)
 
-model = Model(inputs=[premise,hypothesis], outputs=prediction)
-model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+model = Model(inputs=[premise, hypothesis], outputs=pred)
+model.compile(optimizer=OPTIMIZER, loss='categorical_crossentropy', metrics=['accuracy'])
 
 model.summary()
-logging.info('Number of trainable parameters: ' + str(int(np.sum([K.count_params(p) for p in set(model.trainable_weights)]))))
 
-class LossHistory(keras.callbacks.Callback):
-    def on_epoch_end(self, batch, logs={}):
-        print(str(logs))
-        loss = logs.get('loss')
-        val_acc = logs.get('val_acc')
-        acc = logs.get('acc')
-        logging.info("Epoch: " + str(batch) +"/" + str(EPOCHS)  + " ACC: " + str(acc) + " LOSS: " + str(loss) + " VAL_ACC: " + str(val_acc))
-
-logging.info('Training')
+print(" Training")
 _, tmpfn = tempfile.mkstemp()
 # Save the best model during validation and bail out of training early if we're not improving
-callbacks = [EarlyStopping(patience=PATIENCE), ModelCheckpoint(tmpfn, save_best_only=True, save_weights_only=True), LossHistory()]
-model.fit([training[0], training[1]], training[2], batch_size=BATCH_SIZE, epochs=EPOCHS, validation_data=([validation[0], validation[1]], validation[2]), callbacks=callbacks, verbose=2)
+early_stop = EarlyStopping(patience=PATIENCE)
+check_point = ModelCheckpoint(tmpfn, save_best_only=True, save_weights_only=True)
+loss_history = LossHistory()
+callbacks = [early_stop, check_point, loss_history]
+model.fit([training[0], training[1]], training[2], batch_size=BATCH_SIZE, epochs=MAX_EPOCHS, validation_data=([validation[0], validation[1]], validation[2]), callbacks=callbacks, verbose=VERBOSE)
 
 # Restore the best found model during validation
 model.load_weights(tmpfn)
 
 loss, acc = model.evaluate([test[0], test[1]], test[2], batch_size=BATCH_SIZE)
+print(' Test loss / test accuracy = {:.4f} / {:.4f}'.format(loss, acc))
 
-logging.info('Test loss / test accuracy = {:.4f} / {:.4f}'.format(loss, acc) + '\n')
+ops = ""
+for op in [args.agg_ae, args.agg_we, args.align_op_ae, args.align_op_we]:
+    if op is None: continue
+    ops += op
 
+name = os.path.splitext(os.path.basename(args.embedding))[0]
+pred_file = open("pred-{0}-.txt".format(name, ), "w")
+y_proba = model.predict([test[0], test[1]])
+for pred in y_proba:
+    pred = np.argmax(pred)
+    if (pred == 0):
+        pred_file.write("contradiction" + "\n")
+    elif (pred == 1):
+        pred_file.write("neutral" + "\n")
+    elif (pred == 2):
+        pred_file.write("entailment" + "\n")
+
+pred_file.close()
 
 stop = timeit.default_timer()
 time = (stop - start)/60
-logging.info("TIME TAKEN : " + str(time) + " mins")
+
+print(" Model trained in {} mins".format(str(time)))
